ba2f1d3f81891fa2a9ccedf0bd324e7dc1b5342c
Jeremy Glenn <jeremykglenn@gmail.com>
2016-09-28 21:47:46 +0000

    Adds conditionals excercises

---
+++ iceCreamFlavours.js
@@ -0,0 +1,18 @@
+iceCreamFlavours
+
+// Do not modify these first two lines
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+"iceCreamFlavours at index 0: " + iceCreamFlavours[0];
+console.log("chocolate");
+
+"iceCreamFlavours at index 5: " + iceCreamFlavours[5];
+console.log("root beer");
+
+"iceCreamFlavours.length";
+console.log("iceCreamFlavours");
